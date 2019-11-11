50ade420eb8d50d51ae9d68229959ee7968c7cab
Melisa Brown <melisa@melisas-mbp.localdomain>
2019-11-11 18:36:15 -0500

    prepared config files for deployment

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -32,7 +32,7 @@ config :time2, Time2Web.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :time2, Time2Web.Endpoint, server: true
+config :time2, Time2Web.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
